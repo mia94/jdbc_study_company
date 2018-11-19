78fecab5d140528d9669d03f5539414fc4de840c
bonnie <bonnie@DESKTOP-V5VBT80>
2018-11-19 20:47:25 +0900

    Main 정리

--- src/main/java/jdbc_study_company/ui/TitleManagementUi.java
+++ src/main/java/jdbc_study_company/ui/TitleManagementUi.java
@@ -20,22 +20,6 @@ public class TitleManagementUi extends JFrame {
 	private JTextField tfTname;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TitleManagementUi frame = new TitleManagementUi();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public TitleManagementUi() {
