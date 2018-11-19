7fb7977622d96a5a2722901f56989093e80b73d1
bonnie <bonnie@DESKTOP-V5VBT80>
2018-11-19 20:45:42 +0900

    connectionProvider, LogUtil

---
+++ src/test/java/jdbc_study_company/ConnectionProviderTest.java
@@ -0,0 +1,44 @@
+package jdbc_study_company;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import jdbc_study_company.jdbc.ConnectionProvider;
+import jdbc_study_company.jdbc.LogUtil;
+
+public class ConnectionProviderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("Start ConnectionProviderTest");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("End ConnectionProviderTest");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println();
+	}
+
+	@Test
+	public void SelectProductTest() {
+		try(Connection connection = ConnectionProvider.getConnection()){
+			LogUtil.prnLog(connection.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
